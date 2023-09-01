7e539302b6aa0195ba0d5df4cee1cf4f94ae48f0
Deepak Guptha <vzan2012@gmail.com>
2023-09-01 16:17:26 +0530

    feat: added maps library

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import Footer from "./components/Layouts/Footer/Footer";
 import Header from "./components/Layouts/Header/Header";
 import Maps from "./components/Maps/Maps";
 
-function App() {
-  const controller = new AbortController();
+const fetchURL = import.meta.env.VITE_NASA_EONET_URL;
+const controller = new AbortController();
+
+const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header title="Natural Event Tracker" />
-      <Maps />
+      <Maps sourceObject={{ fetchURL, controller: controller.signal }} />
       <Footer url="https://github.com/vzan2012" username="vzan2012" />
-    </>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,24 +1,147 @@
 import { Box, SimpleGrid } from "@chakra-ui/react";
 import classes from "./Maps.module.css";
 
-import GoogleMapReact from "google-map-react";
+import LocationMarker from "../Location/LocationMarker/LocationMarker";
 
-const Maps = () => {
-  const defaultMapProps = {
-    zoom: 6,
-    center: {
-      lat: 10.99,
-      lng: 77.01,
+import { fetchEventsData } from "../../utils/http";
+import { useQuery } from "@tanstack/react-query";
+import * as _ from "lodash";
+import { Map, Marker, Overlay, ZoomControl } from "pigeon-maps";
+import { Icon } from "@iconify/react";
+
+const currentDate = new Date();
+
+const formatEventsData = (eventsData) => {
+  return _.chain(eventsData)
+    .map((event) => ({
+      ...event,
+      geometry: event.geometry.filter(
+        (todayEvent) =>
+          todayEvent.date.split("T")[0] ===
+          currentDate.toISOString().split("T")[0]
+      ),
+    }))
+    .filter((event) => event.geometry.length !== 0)
+    .map((event) => ({
+      ...event,
+      geometry: _.chain(event.geometry).last().value(),
+    }))
+    .value();
+};
+
+const Maps = ({ sourceObject }) => {
+  let latestEvents, markers;
+  const { fetchURL, controller } = sourceObject;
+  const {
+    data: eventsData,
+    isError,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["events"],
+    queryFn: () =>
+      fetchEventsData({
+        fetchURL: `${fetchURL}/events`,
+        controller,
+      }),
+  });
+
+  const iconNameSet = {
+    drought: {
+      name: "carbon:drought",
+      color: "#333",
+    },
+    dustHaze: {
+      name: "ri:haze-2-line",
+      color: "#5D4037",
+    },
+    earthquakes: {
+      name: "ri:earthquake-line",
+      color: "#FF5252",
+    },
+    floods: {
+      name: "ic:outline-flood",
+      color: "#3f51b5",
+    },
+    landslides: {
+      name: "mdi:landslide",
+      color: "#212121",
+    },
+    manmade: {
+      name: "game-icons:human-target",
+      color: "#ff5722",
+    },
+    seaLakeIce: {
+      name: "game-icons:frozen-orb",
+      color: "#448aff",
+    },
+    severeStorms: {
+      name: "wi:wu-chancetstorms",
+      color: "#FF5722",
+    },
+    snow: {
+      name: "wpf:snow",
+      color: "#448aff",
+    },
+    tempExtremes: {
+      name: "iconoir:temperature-high",
+      color: "#E91E63",
+    },
+    volcanoes: {
+      name: "fa6-solid:volcano",
+      color: "#d32f2f",
+    },
+    waterColor: {
+      name: "material-symbols:water-ph-outline",
+      color: "#009688",
+    },
+    wildfires: {
+      name: "mdi:fire-alert",
+      color: "#FF5722",
     },
   };
+
+  console.log("DATA - S");
+
+  if (!isLoading) {
+    latestEvents = formatEventsData(eventsData);
+    console.log(latestEvents);
+  }
+
+  if (latestEvents) {
+    markers = latestEvents.map((event, index) => {
+      console.log(event.geometry.coordinates[1]);
+      console.log(event.geometry.coordinates[0]);
+      console.log(event.categories[0].id);
+      return (
+        <Overlay
+          key={index}
+          anchor={[
+            event.geometry.coordinates[1],
+            event.geometry.coordinates[0],
+          ]}
+          offset={[0, 0]}
+        >
+          <Icon
+            icon={iconNameSet[event.categories[0].id].name}
+            color={iconNameSet[event.categories[0].id].color}
+            className="map-location-icon"
+            fontSize={"30px"}
+          />
+        </Overlay>
+      );
+    });
+  }
+  console.log(markers);
+  console.log("DATA - E");
+
   return (
     <SimpleGrid className={classes["map-section"]}>
       <Box className={classes.maps}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "" }}
-          center={defaultMapProps.center}
-          zoom={defaultMapProps.zoom}
-        ></GoogleMapReact>
+        <Map defaultCenter={[50.879, 4.6997]} defaultZoom={3}>
+          <ZoomControl />
+          {markers}
+        </Map>
       </Box>
     </SimpleGrid>
   );
